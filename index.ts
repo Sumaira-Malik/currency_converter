9aac872ac6e4a4caac0b57cffb975a2d62480a78
Sumaira-Malik <147874280+Sumaira-Malik@users.noreply.github.com>
2024-07-14 21:58:25 +0500

    push my project on githup

---
+++ index.ts
@@ -0,0 +1,59 @@
+#! /usr/bin/env node 
+
+
+import chalk from "chalk";
+
+import inquirer from "inquirer";
+
+const currency : any = {
+    USD : 1, // Base currency
+    EUR : 0.917,
+    PKR : 278.28,
+    IND : 83.53,
+    AED : 3.673
+};
+
+
+
+// Making questions
+
+let user_answer = await inquirer.prompt([
+    {
+        name : 'From',
+        message : chalk.cyanBright('Enter From Currency'),
+        Type : 'List',
+        choices : ['USD','EUR','PKR','IND','AED']
+    },
+    {
+        name : 'to',
+        message : chalk.blue('Enter To Currency'),
+        type : 'list',
+        choices : ['USD','EUR','PKR','IND','AED']
+    },
+    {
+        name : 'amount',
+        message : chalk.yellow('Enter Your Amount'),
+        type : 'number'
+    }
+])
+
+let  fromAmount = currency[user_answer.From] //exchange Rate
+
+let  toAmount = currency[user_answer.to] // exchange rate
+
+let amount = user_answer.amount
+
+let baseAmount = amount / fromAmount // usd base currency // 4
+
+let convertedAmount = baseAmount * toAmount
+
+console.log(chalk.gray(convertedAmount));
+
+
+
+
+
+
+
+
+
